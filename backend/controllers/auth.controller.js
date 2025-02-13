b8b0cc48735b59677c8dd923dfa032b2d19e64ef
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-13 23:11:02 +0600

    feat(user) - welcome email sending implementation using mailtrap

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 export const signup = async (req, res) => {
 	try {
@@ -46,6 +47,13 @@ export const signup = async (req, res) => {
 
 		res.status(201).json({ message: "User registered successfully" });
 
+        const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
+
+		try {
+			await sendWelcomeEmail(user.email, user.name, profileUrl);
+		} catch (emailError) {
+			console.error("Error sending welcome Email", emailError);
+		}
 	} catch (error) {
 		console.log("Error in signup: ", error.message);
 		res.status(500).json({ message: "Internal server error" });
