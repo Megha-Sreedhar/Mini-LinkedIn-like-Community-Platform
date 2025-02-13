362437cd081439b35d6164194f5919e90b6f9695
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-13 16:18:43 +0600

    feat(dev) - basic authentication route setup with database connection

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-    console.log('Seems like someone made a request');
-});
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
-})
+    connectDB();
+});
