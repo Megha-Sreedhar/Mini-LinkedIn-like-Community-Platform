4b04df66af93dadf299f6b0a4e786684f47530e2
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-13 17:46:37 +0600

    feat(dev) - signup api endpoint implemented

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
+    console.log(`Server is listening on ${PORT}....`);
     connectDB();
 });
