e2d3396f072bda7c452519c6be90ffc0a8608710
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-14 11:40:28 +0600

    feat(user) - api endpoint for users public profile and suggestions added

---
+++ backend/routes/user.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getSuggestedConnections, getPublicProfile } from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.get('/suggestions', protectRoute, getSuggestedConnections);
+router.get("/:username", protectRoute, getPublicProfile);
+
+export default router;
