d5ef246217fac0bf94fb2e16246fbfb38d75394a
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-15 13:54:21 +0600

    feat(frontend) - basic auth router setup with react query done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage/HomePage";
+import SignUpPage from "./pages/auth/SignUpPage/SignUpPage";
+import LoginPage from "./pages/auth/LoginPage/LoginPage";
 
 export default function App() {
   return (
     <Layout>
-      Hello World
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
     </Layout>
   )
 }
