b8315f3ef1578a996a364888625718d06dae5061
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-15 15:34:07 +0600

    feat(frontend) - signup functionality implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage/HomePage";
 import SignUpPage from "./pages/auth/SignUpPage/SignUpPage";
 import LoginPage from "./pages/auth/LoginPage/LoginPage";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/signup" element={<SignUpPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
       </Routes>
+      <Toaster />
     </Layout>
   )
 }
