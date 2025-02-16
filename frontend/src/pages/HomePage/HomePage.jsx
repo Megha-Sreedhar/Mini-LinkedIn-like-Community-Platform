f32bcdd51d6a855edde133f40253f44cebbbe901
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-16 17:46:05 +0600

    feat(frontend) - post interaction and recommended users section added

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "../../lib/axios";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import PostCreation from "../../components/PostCreation/PostCreation";
+import Post from "../../components/Post/Post";
+import { Users } from "lucide-react";
+import RecommendedUser from "../../components/RecommendedUser/RecommendedUser";
 
 const HomePage = () => {
     const { data: authUser } = useQuery({ queryKey: ["authUser"] });
@@ -33,7 +36,32 @@ const HomePage = () => {
 
             <div className="col-span-1 lg:col-span-2 order-first lg:order-none">
                 <PostCreation user={authUser}></PostCreation>
+
+                {posts?.map((post) => (
+					<Post key={post._id} post={post} />
+				))}
+
+                {posts?.length === 0 && (
+					<div className='bg-white rounded-lg shadow p-8 text-center'>
+						<div className='mb-6'>
+							<Users size={64} className='mx-auto text-blue-500' />
+						</div>
+						<h2 className='text-2xl font-bold mb-4 text-gray-800'>No Posts Yet</h2>
+						<p className='text-gray-600 mb-6'>Connect with others to start seeing posts in your feed!</p>
+					</div>
+				)}
             </div>
+
+            {recommendedUsers?.length > 0 && (
+				<div className='col-span-1 lg:col-span-1 hidden lg:block'>
+					<div className='bg-secondary rounded-lg shadow p-4'>
+						<h2 className='font-semibold mb-4'>People you may know</h2>
+						{recommendedUsers?.map((user) => (
+							<RecommendedUser key={user._id} user={user} />
+						))}
+					</div>
+				</div>
+			)}
         </div>
     )
 }
